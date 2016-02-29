2c39bf4a0c2d1bed0a7cacdd7d2665dfa6700dfb
Anna Kelly <annakelly@Davids-MacBook-Pro.local>
2016-02-29 10:46:07 -0500

    created 'points' branch to work on figuring out the game points system, eg: hp, ap, and final score points

---
+++ model.py
@@ -0,0 +1,71 @@
+import random
+
+
+class Character:
+    """
+    name = name of character
+    hp = health points
+    atk = attack points
+    score = initial score, defaults to zero
+    """
+
+    def __init__(self, name, hp, atk, score=0):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.score = score
+
+    def attack(self):
+        """
+        Returns a damage amount to enemy
+        """
+        rand = random.randint(0, 20)
+        # serves as a 20 side dice roll for random/chance
+        if rand > 7:
+            return int(self.atk * 2)
+        return 0
+
+    def take_damage(self, damage):
+        """
+        Affects current user/player. If player/user hp is
+        less or equal to zero (ie. if a enemy attacks 20 points and the user only has 10 hp,
+        it results in -10 hp) they are killed
+        """
+        self.hp -= damage
+        print("{} has lost {} health".format(self.name, damage))
+        return self.hp
+
+    def is_dead(self):
+        """
+        Returns boolean saying when character is dead.
+        """
+        return self.hp <= 0
+
+    def add_points(self, amount): # calculate function for adding game points
+        self.score += amount
+        return self.score   # used to display the final game points score
+
+
+class Player(Character):
+
+    def pick_weapon(self):
+        user_pick = input("Pick a weapon (1, 2, 3)")
+        if user_pick == "1":
+            self.atk += 20
+            print("You have chosen the Dwarven-forged battle axe Jarnbjorn, Wrecker of Worlds as your weapon. Your attack power has increased by 20 points")
+        if user_pick == "2":
+            self.atk += 15
+            print("You have donned Megingjord, the belt of strength. Your attack power has increased by 15 points")
+        if user_pick == "3":
+            self.atk += 50
+            print("You have chosen Mjolnir, imbued with the might of Thor and created using the core of a star. Your attack power has increased by 50")
+        else:
+            print("Pick a weapon (1, 2, 3)")
+
+
+class Enemy(Character):
+    pass
+    # self.name = name
+    # self.hp = hp
+    # self.ap = ap
+    # self.score = score
---
+++ view.py
@@ -0,0 +1,41 @@
+class View:
+
+    def backstory(self):
+        input("""
+            In the other-dimensional realm of Asgard, Thor -- the god of thunder and ruler of Asgard --
+            and his people have spent their lives fighting the demonic creatures that constantly try to
+            invade their home.
+            (Press 'Enter' to continue)""")
+        print()
+        input("""
+            As the firstborn son of King Odin and goddess Gaea, Thor is the strongest of the Asgardians.
+            He is a superb hand-to-hand combatant, and is skilled in armed combat.
+            He possesses three items which assist him in combat: the battle axe Jarnbjorn,
+            the enchanted Belt of Strength, and his signature weapon, the mystical hammer Mjolnir, which
+            grants him the ability of flight and weather manipulation. Thor can also summon the elements
+            of the storm - lightning, rain, wind, and snow.
+            (Press 'Enter' to continue)""")
+        print()
+        input("""
+            Thor’s sworn enemies are the Destroyer Armor, Absorbing Man and Amora.
+            (Press 'Enter' to continue)""")
+        print()
+        input("""
+            In addition to the Destory Armor's fight capabilities, invulnerability,
+            incredible strength, the Destory Armor's energy blasts can shatter Thor’s hammer.
+            The Destroyer requires a living soul to inhabit it as a pilot; but the longer
+            that person inhabits the armor, the more corrupt they become.
+            (Press 'Enter' to continue)""")
+        print()
+        input("""
+            The Absorbing Man is an ex-boxer armed with superhuman abilities. He is able increase
+            his mass while mimicking the properties of any substance he touches.
+            (Press 'Enter' to continue)""")
+        print()
+        input("""
+            Amora is the most powerful sorceress in Asgard an has an unhealthy obsession to
+            bring Thor under her power. Over the years, she used her talent for magic and
+            manipulation to aid her evil ways to fight Thor and his people. She even hypnotized
+            Thor’s friends into turning against him, and has often tricked others into acting as
+            her agents. (Press 'Enter' to continue)""")
+        print()
